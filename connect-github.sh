e75ce247904be8c6a1107a2dad9a92a083bca7fd
Seo, In Seog <sis6789@gmail.com>
2026-07-16 08:04:06 +0900

    vault backup: 2026-07-16 08:04:06

---
+++ connect-github.sh
@@ -0,0 +1,7 @@
+echo "# codyssey-b13" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sis6789/codyssey-b13.git
+git push -u origin main
